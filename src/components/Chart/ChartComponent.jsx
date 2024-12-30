0e679912d1b0ac3df324246f432b3d2dc41b9a99
hairyan456 <hairyan789@gmail.com>
2024-12-30 18:11:08 +0700

    Custom zing chart (2)

--- src/components/Chart/ChartComponent.jsx
+++ src/components/Chart/ChartComponent.jsx
@@ -1,54 +1,73 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import { Line } from 'react-chartjs-2';
 import { Chart } from 'chart.js/auto';
 import { useSelector } from 'react-redux';
-
-const options = {
-    responsive: true,
-    pointRadius: 0,
-    aspectRatio: 4,
-    scales: {
-        y: {
-            ticks: { display: false },
-            grid: { borderDash: [1, 4], color: 'gray' }
-        },
-        x: {
-            ticks: { color: 'white' },
-            grid: { color: 'transparent' }
-        }
-    },
-    plugins: {
-        legend: false
-    }
-}
+import { SongItem } from '../../modules/Home';
 
 const ChartComponent = ({ className = '' }) => {
     const chart = useSelector(state => state.app?.chart);
     const rank = useSelector(state => state.app?.rank);
-
     const [data, setData] = useState(null);
+
+    const options = useMemo(() => ({
+        responsive: true,
+        pointRadius: 0,
+        maintainAspectRatio: false,
+        scales: {
+            y: {
+                ticks: { display: false },
+                grid: { color: 'rgba(255,255,255,0.2)', drawTicks: false },
+                min: chart?.minScore,
+                max: chart?.maxScore,
+                border: { dash: [3, 4] },
+            },
+            x: {
+                ticks: { color: 'white' },
+                grid: { color: 'transparent' }
+            }
+        },
+        plugins: {
+            legend: false
+        },
+        hover: {
+            mode: 'dataset',
+            intersect: false
+        }
+    }), []);
 
     useEffect(() => {
         if (chart?.times && chart?.items) {
-            const labels = chart.times.filter(item => +item.hour % 2 === 0).map(item => item.hour);
+            const labels = chart.times.filter(item => +item.hour % 2 === 0).map(item => `${item.hour}:00`);
             const datasets = Object.keys(chart.items).slice(0, 3).map((key, i) => ({
                 data: chart.items[key].filter(item => +item.hour % 2 === 0).map(item => item.counter),
-                borderColor: ['blue', 'yellow', 'red'][i], // Lấy màu theo chỉ số
-                tension: 0.2,
-                borderWidth: 2
+                borderColor: i === 0 ? '#4a90e2' : i === 1 ? '#50e3c2' : '#e35050',
+                tension: 0.3,
+                borderWidth: 2,
+                pointHoverRadius: 5,
+                pointBackgroundColor: 'white',
+                pointHitRadius: 4,
+                pointBorderColor: i === 0 ? '#4a90e2' : i === 1 ? '#50e3c2' : '#e35050',
+                animation: false,
+                pointHoverBorderWidth: 4
             }));
             setData({ labels, datasets });
         }
     }, [chart]);
 
-
     return (
         <div className={`${className} `}>
             <div className='p-5 flex flex-col gap-3 rounded-md bg-[rgba(115,20,140,0.8)]'>
                 <h3 className='font-semibold text-xl text-white'>#ZingChart</h3>
-                <div className='flex gap-4'>
-                    <div className='basis-1/3  border border-white'>rank</div>
-                    <div className='basis-2/3 '>
+                <div className='flex gap-3'>
+                    <div className='basis-1/3 flex flex-col gap-3'>
+                        {rank?.length > 0 && rank.filter((item, index) => index < 3).map((item, index) => (
+                            <SongItem key={item?.encodeId} data={item}
+                                order={index + 1}
+                                percent={Math.round((item?.score / chart?.totalScore) * 100)}
+                            />
+                        ))}
+                    </div>
+                    <div className='basis-2/3'>
                         {data && <Line data={data} options={options} />}
                     </div>
                 </div>
--- src/modules/Home/SongItem.jsx
+++ src/modules/Home/SongItem.jsx
@@ -6,7 +6,7 @@ import * as actions from '../../redux/action'
 
 moment.locale('vi'); // Thiết lập ngôn ngữ mặc định là tiếng Việt
 
-const SongItem = ({ data = {} }) => {
+const SongItem = ({ data = {}, ...props }) => {
     const dispatch = useDispatch();
 
     const playSong = (sId) => {
@@ -15,22 +15,34 @@ const SongItem = ({ data = {} }) => {
     };
 
     return (
-        <div className='w-full flex-auto flex p-[10px] gap-3 cursor-pointer hover:rounded-md hover:bg-E7 hover:transition-colors 
-        hover:ease-in-out hover:duration-500'
+        <div className={`w-full flex-auto flex p-[10px] items-center justify-between cursor-pointer hover:rounded-md hover:transition-colors 
+        hover:ease-in-out hover:duration-300 
+        ${props?.order ? 'text-white bg-[hsla(0,0%,100%,.07)] hover:bg-[#A874B8]' : 'text-black hover:bg-E7'}`}
             onClick={() => playSong(data?.encodeId)}
         >
-            <img src={data?.thumbnail} alt="thumbnail" className='w-10 h-10 xl:w-[60px] xl:h-[60px] object-cover rounded-md' />
-            <div className='flex flex-col gap-1'>
-                <div className='text-[12px] lg:text-sm font-semibold text-gray-800'>
-                    {data?.title?.length > 40 ? data.title.slice(0, 39) + '...' : data.title}
-                </div>
-                <div className='text-[10px] lg:text-xs font-light'>
-                    {data?.artistsNames?.length > 30 ? data.artistsNames.slice(0, 29) + '...' : data.artistsNames}
-                </div>
-                <div className='text-[10px] lg:text-xs font-light'>
-                    {moment(data?.releaseDate * 1000).fromNow()}
+            <div className='flex gap-3 items-center'>
+                {props?.order &&
+                    <span className={`text-[30px] text-white drop-shadow-md`}>
+                        {props.order}
+                    </span>
+                }
+                <img src={data?.thumbnail} alt="thumbnail"
+                    className={`w-10 h-10 ${props?.order ? 'xl:w-[40px] xl:h-[40px]' : 'xl:w-[60px] xl:h-[60px]'} object-cover rounded-md`} />
+                <div className='flex flex-col gap-1'>
+                    <div className={`text-[12px] lg:text-sm font-semibold  ${!props?.order && 'text-gray-700'}`}>
+                        {data?.title?.length > 40 ? data.title.slice(0, 39) + '...' : data.title}
+                    </div>
+                    <div className='text-[10px] lg:text-xs font-light'>
+                        {data?.artistsNames?.length > 30 ? data.artistsNames.slice(0, 29) + '...' : data.artistsNames}
+                    </div>
+                    {!props?.order &&
+                        <div className='text-[10px] lg:text-xs font-light'>
+                            {moment(data?.releaseDate * 1000).fromNow()}
+                        </div>
+                    }
                 </div>
             </div>
+            {props?.percent && <span>{props.percent}%</span>}
         </div>
     );
 };
